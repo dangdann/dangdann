19dda98934a58066212771e797fe09a72d438191
dangdann <140431743+dangdann@users.noreply.github.com>
2023-07-24 17:33:26 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,9 @@
+- 👋 Hi, I’m @dangdann
+- 👀 I’m interested in video editing 
+- 💞️ I’m looking to collaborate on any wide or small scale video
+- 📫 How to reach me 07041344217
+
+<!---
+dangdann/dangdann is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
